c2e2656326f4ee3b366cb62e43e28cbea6a1d3b4
Sjors Gielen <sjors@sjorsgielen.nl>
2016-07-26 17:07:05 +0200

    Fix warning on CMake 3.6.0+

--- cmake/Toolchain-i686-cloudabi.cmake
+++ cmake/Toolchain-i686-cloudabi.cmake
@@ -1,6 +1,10 @@
-INCLUDE(CMakeForceCompiler)
-
 SET(CMAKE_SYSTEM_NAME Generic)
 
-CMAKE_FORCE_C_COMPILER(i686-unknown-cloudabi-cc Clang)
-CMAKE_FORCE_CXX_COMPILER(i686-unknown-cloudabi-c++ Clang)
+if(CMAKE_VERSION VERSION_LESS "3.6.0")
+	INCLUDE(CMakeForceCompiler)
+	CMAKE_FORCE_C_COMPILER(i686-unknown-cloudabi-cc Clang)
+	CMAKE_FORCE_CXX_COMPILER(i686-unknown-cloudabi-c++ Clang)
+else()
+	set(CMAKE_C_COMPILER i686-unknown-cloudabi-cc)
+	set(CMAKE_CXX_COMPILER i686-unknown-cloudabi-c++)
+endif()
