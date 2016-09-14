846370b95b4e4e035a16f5d6c72035fabcfd74a2
Sjors Gielen <sjors@sjorsgielen.nl>
2016-09-14 22:36:02 +0200

    Add socket_fd, a pipe-like fd but for bidirectional communication.

---
+++ fd/socket_fd.cpp
@@ -0,0 +1,37 @@
+#include "socket_fd.hpp"
+#include "pipe_fd.hpp"
+#include <memory/allocator.hpp>
+
+using namespace cloudos;
+
+socket_fd::socket_fd(pipe_fd *r, pipe_fd *w, const char *n)
+: fd_t(CLOUDABI_FILETYPE_SOCKET_STREAM, n)
+, readfd(r)
+, writefd(w)
+{
+}
+
+size_t socket_fd::read(size_t offset, void *dest, size_t count)
+{
+	return readfd->read(offset, dest, count);
+}
+
+error_t socket_fd::putstring(const char *str, size_t count)
+{
+	return writefd->putstring(str, count);
+}
+
+error_t socket_fd::socketpair(socket_fd **a, socket_fd **b, size_t capacity)
+{
+	pipe_fd *tx = get_allocator()->allocate<pipe_fd>();
+	pipe_fd *rx = get_allocator()->allocate<pipe_fd>();
+	new (tx) pipe_fd(capacity, "tx for socket");
+	new (rx) pipe_fd(capacity, "rx for socket");
+
+	*a = get_allocator()->allocate<socket_fd>();
+	*b = get_allocator()->allocate<socket_fd>();
+	new (*a) socket_fd(tx, rx, "socketpair tx socket");
+	new (*b) socket_fd(rx, tx, "socketpair rx socket");
+
+	return error_t::no_error;
+}
---
+++ fd/socket_fd.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "fd.hpp"
+
+namespace cloudos {
+
+struct pipe_fd;
+
+/**
+ * A socket FD.
+ *
+ * This FD consists of a pipe (fifo) FD to read from, and one to write to.
+ * This way, it implements bidirectional communication using the FIFO building
+ * block. Use the socketpair() factory function to build a complete socket
+ * pair from scratch.
+ */
+struct socket_fd : fd_t {
+	socket_fd(pipe_fd *read, pipe_fd *write, const char *n);
+
+	/** read() blocks until at least 1 byte of data is available;
+	 * then, it returns up to count bytes of data in the dest buffer.
+	 * It sets invalid_argument as the error if offset is not 0.
+	 */
+	size_t read(size_t offset, void *dest, size_t count) override;
+
+	/** putstring() blocks until there is capacity for at least count
+	 * bytes, then, it appends the given buffer to the stored one.
+	 */
+	error_t putstring(const char * /*str*/, size_t /*count*/) override;
+
+	/** Creates two socket_fd's that form a connected pair. */
+	static error_t socketpair(socket_fd **a, socket_fd **b, size_t capacity);
+
+private:
+	pipe_fd *readfd;
+	pipe_fd *writefd;
+};
+
+}
