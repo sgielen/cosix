0402b531465f83c38851eccc7c090fa4e861934b
Sjors Gielen <sjors@sjorsgielen.nl>
2016-10-19 15:28:45 +0200

    Re-activate unit tests again.
    When all failing unit tests are disabled, 640 out of 890 already succeed. Out
    of the 250 failing ones, most fail because of the following reasons:
    - clocks aren't implemented
    - you can't poll() for process termination
    - fd_seek() is not implemented
    - fd_stat_put() is not implemented
    - sockets are not supported
    - errors are not correctly returned to the userland
    - shared mappings are not supported
    - various system calls are not implemented

--- userland/init/init.cpp
+++ userland/init/init.cpp
@@ -90,13 +90,13 @@ void program_main(const argdata_t *) {
 
 	/*start_binary("exec_test");
 	start_binary("thread_test");
-	start_binary("pipe_test");*/
-	start_binary("concur_test");
+	start_binary("pipe_test");
+	start_binary("concur_test");*/
 
-	//start_tmpfs();
+	start_tmpfs();
 	//start_binary("tmptest");
 
-	//start_unittests();
+	start_unittests();
 
 	// init must never exit, but we don't have sys_poll() / sys_poll_fd()
 	// yet, so we need to busy-wait
