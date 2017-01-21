5723a35f21dbdbf5d4316c5788658bfe03453350
Sjors Gielen <sjors@sjorsgielen.nl>
2017-01-22 00:23:05 +0100

    Improve comments in the linear congruential generator.

--- rng/rng.hpp
+++ rng/rng.hpp
@@ -15,7 +15,9 @@ struct rng {
 	void get(char *str, size_t length);
 
 private:
-	/* const values taken from glib */
+	/* m and c must be relative primes (gcd is 1), all prime factors of m
+	 * divide a - 1; these values are taken from glibc
+	 */
 	static const uint32_t a = 1103515245;
 	static const uint64_t m = 1ull << 32;
 	static const uint32_t c = 12345;
