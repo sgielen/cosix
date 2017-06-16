2c1167cc1f8fbe3ceea8677e1ab2a8f824f4d992
Sjors Gielen <sjors@sjorsgielen.nl>
2017-06-16 03:01:40 +0200

    Use the same toolchain files as in arpc, including a workaround for a bug in clang.

--- cmake/Toolchain-i686-cloudabi.cmake
+++ cmake/Toolchain-i686-cloudabi.cmake
@@ -1,10 +1,12 @@
+cmake_minimum_required(VERSION 3.6.0)
 SET(CMAKE_SYSTEM_NAME Generic)
 
-if(CMAKE_VERSION VERSION_LESS "3.6.0")
-	INCLUDE(CMakeForceCompiler)
-	CMAKE_FORCE_C_COMPILER(i686-unknown-cloudabi-cc Clang)
-	CMAKE_FORCE_CXX_COMPILER(i686-unknown-cloudabi-c++ Clang)
-else()
-	set(CMAKE_C_COMPILER i686-unknown-cloudabi-cc)
-	set(CMAKE_CXX_COMPILER i686-unknown-cloudabi-c++)
+set(CLOUDABI_TRIPLET i686-unknown-cloudabi)
+include(${CMAKE_CURRENT_LIST_DIR}/Toolchain-cloudabi-common.cmake)
+
+# Work-around for clang r264966
+if(NOT CLOUDABI_NO_X86_CALL_FRAME_SET)
+	set(CLOUDABI_NO_X86_CALL_FRAME_SET 1)
+	set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -mllvm -no-x86-call-frame-opt")
+	set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -mllvm -no-x86-call-frame-opt")
 endif()
