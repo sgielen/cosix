e8e612e7ffbc97470b716140a0bc2e7bd351dae7
Sjors Gielen <sjors@sjorsgielen.nl>
2017-07-22 14:59:44 +0200

    Forgot to commit the terminal_fd files.

---
+++ term/terminal_fd.cpp
@@ -0,0 +1,33 @@
+#include <term/terminal_fd.hpp>
+
+using namespace cloudos;
+
+terminal_fd::terminal_fd(shared_ptr<terminal> t)
+: terminal_fd(t, nullptr)
+{
+}
+
+terminal_fd::terminal_fd(shared_ptr<terminal> t, const char *n)
+: fd_t(CLOUDABI_FILETYPE_CHARACTER_DEVICE, n ? n : "")
+, term(t)
+{
+	if(n == nullptr) {
+		strncpy(name, "terminal_fd to ", sizeof(name));
+		strlcat(name, t->get_name(), sizeof(name));
+	}
+}
+
+terminal_fd::~terminal_fd()
+{}
+
+size_t terminal_fd::read(void *dest, size_t count)
+{
+	error = term->read_keystrokes(reinterpret_cast<char*>(dest), &count);
+	return error == 0 ? count : 0;
+}
+
+size_t terminal_fd::write(const char *str, size_t count)
+{
+	error = term->write_output(str, count);
+	return error == 0 ? count : 0;
+}
---
+++ term/terminal_fd.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <fd/fd.hpp>
+#include <term/terminal.hpp>
+#include <memory/smart_ptr.hpp>
+
+namespace cloudos {
+
+struct terminal_fd : public fd_t {
+	terminal_fd(shared_ptr<terminal>);
+	terminal_fd(shared_ptr<terminal>, const char *n);
+
+	~terminal_fd() override;
+
+	size_t read(void *dest, size_t count) override;
+	size_t write(const char *str, size_t count) override;
+
+private:
+	shared_ptr<terminal> term;
+};
+
+}
